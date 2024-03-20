c5e803c816c7bfe9f5522b3c6a5d578655565913
ahmadihsanullah <ahma21327ti@student.nurulfikri.ac.id>
2024-03-20 17:29:24 +0700

    add primary bean

--- src/main/java/programmerzamannow/spring/core/PrimaryBeanConfiguration.java
+++ src/main/java/programmerzamannow/spring/core/PrimaryBeanConfiguration.java
@@ -3,19 +3,19 @@ package programmerzamannow.spring.core;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-
 import programmerzamannow.spring.core.Data.Foo;
 
 @Configuration
-public class DuplicateBeanConfiguration {
-    
-    @Bean
-    public Foo foo1(){
-        return new Foo();
-    }
+public class PrimaryBeanConfiguration {
+  @Primary
+  @Bean(value = "fooFirst")
+  public Foo foo1() {
+    return new Foo();
+  }
 
-    @Bean
-    public Foo foo2(){
-        return new Foo();
-    }
+  @Bean(value = "fooSecond")
+  public Foo foo2() {
+    return new Foo();
+  }
 }
+    
